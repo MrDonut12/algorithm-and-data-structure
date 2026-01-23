1e5d0542698fdb956021b0a896479bb56f6f0607
Mr.Donut <KhanhNDCE200196@gmail.com>
2026-01-23 20:01:27 +0700

    modify coe

--- CSES/Sorting and Search/Sum of Two Values.cpp
+++ CSES/Sorting and Search/Sum of Two Values.cpp
@@ -1,25 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll int
-const ll N = 2e6 + 276;
-const ll M = 1e3 + 276;
-ll n ;
-vector <ll> a;
-#define fastIO                                                                 \
-    ios::sync_with_stdio(false);                                               \
-    cin.tie(nullptr);                                                             \
-    cout.tie(nullptr);                                                            \
-    cout.precision(numeric_limits<double>::max_digits10);
-stack <pair <ll , ll> > f;
-int main() {
-    fastIO;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    sort (a.begin(), a.end());
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
+
+#define ll long long
+#define ld long double
+#define ci pair <char, ll>
+#define endl "\n"
+#define deb cout<<"debug\n"
+#define fi first
+#define se second
+#define pb push_back
+#define forto(i, a, b) for(int i = a; i <= b; i++)
+#define fordto(i, a, b) for(int i = a; i >= b; i--)
+#define rep(i, n) for(int i = 1; i <= n; i++)
+#define fastIO ios_base::sync_with_stdio(false);\
+                    cin.tie(0);
+
+using namespace chrono;
+const ll N = 2e5 + 1103;
+const ll M = 1e3 + 1103;
+const ll INF = 1e9 + 7;
+
+
+struct ii {
+    int val, idx;
+};
+
+bool cmp (ii a, ii b) {
+    return a.val < b.val;
 }
 
+
+ii a[N], b[N];
+ll n, x;
+
+ll search(ll target) {
+    ll l = 1, r = n;
+    while (l <= r) {
+        ll g = (l + r) >> 1;
+        if (a[g].val == target) return g;
+        if (a[g].val < target) {
+            l = g + 1;
+        } else {
+            r = g - 1;
+        }
+    }
+    return l;
+}
+
+
+int main() {
+    fastIO;
+    cin >> n >> x;
+    rep(i, n) cin >> a[i].val, a[i].idx = i;;
+    sort(a + 1, a + 1 + n, cmp);
+    rep(i, n) {
+        ll pi = search(x - a[i].val);
+        if (a[pi].val + a[i].val == x && pi != i) {
+            return cout << a[pi].idx << " " << a[i].idx << endl, 0;
+        }
+    }
+
+    return cout << "IMPOSSIBLE" << endl, 0;
+
+}
--- CSES/Sorting and Search/Ticket.cpp
+++ CSES/Sorting and Search/Ticket.cpp
@@ -13,7 +13,8 @@ using namespace std;
 #define forto(i, a, b) for(int i = a; i <= b; i++)
 #define fordto(i, a, b) for(int i = a; i >= b; i--)
 #define rep(i, n) for(int i = 1; i <= n; i++)
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(0);
+#define fastIO ios_base::sync_with_stdio(false);\
+                    cin.tie(0);
 
 using namespace chrono;
 const ll N = 2e5 + 1103;
@@ -21,16 +22,31 @@ const ll M = 1e3 + 1103;
 const ll INF = 1e9 + 7;
 
 ll a[N], b[N];
+multiset <ll> s;
+
 void solve() {
+    s.clear();
+
     int n, m;
     cin >> n >> m;
     rep(i, n) cin >> a[i];
     rep(i, m) cin >> b[i];
-    sort(a + 1, a + 1 + n);
-    sort(b + 1, b + 1 + m);
+    rep(i, n) s.insert(a[i]);
+    rep(i, m) {
+        multiset <ll> :: iterator it = s.upper_bound(b[i]);
+        if (it == s.begin()) {
+            cout << -1 << endl;
+        } else {
+            --it;
+            cout << *it << endl;
+            s.erase(it);
+        }
+    }
+
 }
 
 signed main() {
+    fastIO;
     int tt = 1;
     // cin >> tt;
     while (tt--) {
