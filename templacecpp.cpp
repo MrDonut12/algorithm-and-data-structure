be6fae0c0e2c780960fdd35e31b6ff1d9d42c250
MrDonut12 <khanhNDCE200196@gmail.com>
2026-05-08 18:56:05 +0700

    add template cpp for cp

---
+++ templacecpp.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+#define ll long long
+#define ld long double
+#define ii pair<ll,ll>
+#define fi first
+#define se second
+#define pb push_back
+
+const ll N = 2e5 + 176;
+const ll M = 1e3 + 1103;
+
+ll n, m;
+ll a[N], b[M];
+
+void solve() {
+    cout << "hello world!\n";
+}
+
+int main() {
+    int tt = 1;
+    while (tt--) {
+        solve();
+    }
+}
